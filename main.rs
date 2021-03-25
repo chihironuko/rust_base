611c8ff2e039be5d5b2d6e55bb41d2feb8abc77f
alauda <alauda.jacx@gmail.com>
2021-03-26 01:16:26 +0900

    create main

---
+++ main.rs
@@ -0,0 +1,24 @@
+/*
+avoid keywords "_"
+*/
+use std::any::type_name;
+
+/*
+fn printtype<T>(_:T){
+  println!("{}", type_name::<T>());
+}
+*/
+
+fn printtype<T>(typetest: T){
+  println!("{}", type_name::typetest());
+}
+
+
+fn main(){
+  let character_ = 'H';
+  printtype(character_);
+  let string_ = "hello";
+  printtype(string_);
+  let rawstring_ = r#"hello"#;
+  printtype(rawstring_);
+}
