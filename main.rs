64b0f05e8af4d06f08800001c1f036fc48901cf7
alauda <alauda.jacx@gmail.com>
2021-03-26 01:48:00 +0900

    update main

--- main.rs
+++ main.rs
@@ -3,18 +3,19 @@ avoid keywords "_"
 */
 use std::any::type_name;
 
-/*
 fn printtype<T>(_:T){
   println!("{}", type_name::<T>());
 }
-*/
 
+/*
+//issue:why cant execute?
 fn printtype<T>(typetest: T){
   println!("{}", type_name::typetest());
 }
-
+*/
 
 fn main(){
+  //token type check
   let character_ = 'H';
   printtype(character_);
   let string_ = "hello";
